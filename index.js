f7e63421e81218c5647f7890fdc02883a597eb49
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-05-04 16:33:00 +0300

    added fileHandler

--- index.js
+++ index.js
@@ -3,23 +3,48 @@ import fs from "fs/promises";
 
 const targetPath = path.resolve("downloads");
 
+const dirDict = {
+  documents: [".txt", ".doc", ".pdf", ".docx"],
+  images: [".png", ".jpg", ".webp", ".svg", ".ico"],
+  videos: [".mp3", ".mp4", ".avi", ".mkv"],
+  archives: [".zip", ".rar"],
+  others: [],
+};
+
 async function dirHandler(dirPath) {
   const data = await fs.readdir(dirPath);
-  console.log(data);
 
   const pathList = data.map((el) => path.join(dirPath, el));
-  console.log(pathList);
 
-  const promiseList = pathList.map(async (pathItem) => {
+  const promiseList = pathList.map(async (itemPath) => {
     return {
-      isDir: (await fs.stat(pathItem)).isDirectory(),
-      itemPath: pathItem,
+      isDir: (await fs.stat(itemPath)).isDirectory(),
+      itemPath,
     };
   });
 
   const result = await Promise.allSettled(promiseList);
 
   const dirStat = result.map(({ value }) => value);
+
+  dirStat.forEach((el) => {
+    if (el.isDir) {
+      dirHandler(el.itemPath);
+    } else {
+      fileHandler(el.itemPath);
+    }
+  });
+}
+
+async function fileHandler(filePath) {
+  const { ext } = path.parse(filePath);
+  for (let key in dirDict) {
+    if (dirDict[key].includes(ext)) {
+      // write to dir 'key'
+      return;
+    }
+  }
+  //write to  dir 'other'
 }
 
 dirHandler(targetPath);
